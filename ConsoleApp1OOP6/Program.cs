4e9c26c96afaaf6692994fd2fb27ddbd62052b20
Koko3000onsy <onsykoko79@gmail.com>
2026-03-09 16:00:32 +0200

    Q1

--- ConsoleApp1OOP6/Program.cs
+++ ConsoleApp1OOP6/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Q1
+            // abstraction is the process of hiding the implementation details and showing only the functionality to the user.
+            // It allows us to focus on what the object does instead of how it does it.
+            // In C#, abstraction can be achieved using abstract classes and interfaces.
+            //abstraction hide implementation details and show only functionality to the user.
+            //focus on what the object does instead of how it does it.reduce complexity and increase efficiency.
+            //encapsulation protect data , focus on how data is accessed,use access modifiers to restrict access to data, increse security.
+            //abstraction EX : online shopping website, we only see the products and their details use add to cart pay and checkout 
+            //but we don't know how the website works behind the scenes .
+            //encapsulation EX : your data is securied can show in login , order system .
+            #endregion
         }
     }
 }
